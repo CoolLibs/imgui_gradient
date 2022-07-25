fd297d0acfcd41d9ca45e5b04595065c9d43b053
TaliaKah <anaissaratagougne@tutanota.com>
2022-07-25 12:15:08 +0200

    🤏 [random] Make rand static

--- include/imgui_gradient/random.h
+++ include/imgui_gradient/random.h
@@ -4,7 +4,7 @@
 
 namespace utils {
 
-float rand() // TODO(ASG) the user could choose the generator
+static float rand() // TODO(ASG) the user could choose the generator
 {
     static std::default_random_engine     generator{std::random_device{}()};
     std::uniform_real_distribution<float> distribution{0.f, 1.f};
