d07395346296ab5463f1157c73e598d9d45cb67d
TaliaKah <anaissaratagougne@tutanota.com>
2022-07-31 12:14:09 +0200

    ✨ [GradientOptions] Added

---
+++ include/imgui_gradient/GradientOptions.h
@@ -0,0 +1,20 @@
+#pragma once
+
+typedef int GradientOptions;
+
+enum GradientOptions_ {
+    GradientOptions_None                 = 0,
+    GradientOptions_NoTooltip            = 1 << 1,
+    GradientOptions_NoResetButton        = 1 << 2,
+    GradientOptions_NoLabel              = 1 << 3,
+    GradientOptions_NoRandomMode         = 1 << 4,
+    GradientOptions_NoAddButton          = 1 << 5,
+    GradientOptions_NoLessButton         = 1 << 6,
+    GradientOptions_NoDragSlider         = 1 << 7,
+    GradientOptions_NoColorButton        = 1 << 8,
+    GradientOptions_NoPositionModeCombo  = 1 << 9,
+    GradientOptions_NoInterpolationCombo = 1 << 10,
+    GradientOptions_NoDragDowntoDelete   = 1 << 11,
+    GradientOptions_NoAddAndLessButton   = GradientOptions_NoAddButton | GradientOptions_NoLessButton | GradientOptions_NoColorButton,
+    GradientOptions_NoCombo              = GradientOptions_NoInterpolationCombo | GradientOptions_NoPositionModeCombo,
+};
