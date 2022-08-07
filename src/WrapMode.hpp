98fc03455530ffd5fe43df74b0d439b5b946b887
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-07 13:22:57 +0200

    💡 [WrapMode] Add Documentation

--- src/WrapMode.hpp
+++ src/WrapMode.hpp
@@ -2,11 +2,12 @@
 
 namespace ImGuiGradient {
 
+/// Severals ways to make a `position` relative (between 0.f and 1.f).
 enum class WrapMode {
-    Clamp,
-    Repeat,
-    MirrorClamp,
-    MirrorRepeat
+    Clamp,       /// `position` clamped between 0.f and 1.f.
+    Repeat,      /// Integer part of `position` ignored to get a relative position.
+    MirrorClamp, /// `position` mirrored when odd, then clamp it between 0.f and 1.f.
+    MirrorRepeat /// `position` mirrored when odd, then integer part ignored to to get a relative position.
 };
 
 } // namespace ImGuiGradient
