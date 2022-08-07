8956613832ccd2e619e4db514d782e65cd0f38c8
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-07 10:01:09 +0200

    🤏 [RelativePosition] Move make_relative_position() in cpp

---
+++ src/RelativePosition.cpp
@@ -0,0 +1,34 @@
+#include "RelativePosition.hpp"
+
+namespace ImGuiGradient {
+auto make_relative_position(float position, WrapMode wrap_mode) -> RelativePosition
+{
+    const auto relative_pos = RelativePosition{
+        [&] {
+            switch (wrap_mode)
+            {
+            case WrapMode::Clamp:
+            {
+                return ImClamp(position, 0.f, 1.f);
+            }
+            case WrapMode::Repeat:
+            {
+                return Utils::repeat_position(position);
+            }
+            case WrapMode::MirrorClamp:
+            {
+                return Utils::mirror_clamp_position(position);
+            }
+            case WrapMode::MirrorRepeat:
+            {
+                return Utils::mirror_repeat_position(position);
+            }
+            default:
+                assert(false && "[RelativePosition::make_relative_position] Invalid enum value");
+                return 0.25f;
+            }
+        }()};
+    return relative_pos;
+}
+
+} // namespace ImGuiGradient
--- src/RelativePosition.hpp
+++ src/RelativePosition.hpp
@@ -55,33 +55,6 @@ private:
     float value{0.f};
 };
 
-inline auto make_relative_position(float position, WrapMode wrap_mode) -> RelativePosition
-{
-    const auto relative_pos = RelativePosition{
-        [&] {
-            switch (wrap_mode)
-            {
-            case WrapMode::Clamp:
-            {
-                return ImClamp(position, 0.f, 1.f);
-            }
-            case WrapMode::Repeat:
-            {
-                return Utils::repeat_position(position);
-            }
-            case WrapMode::MirrorClamp:
-            {
-                return Utils::mirror_clamp_position(position);
-            }
-            case WrapMode::MirrorRepeat:
-            {
-                return Utils::mirror_repeat_position(position);
-            }
-            default:
-                assert(false && "[RelativePosition::make_relative_position] Invalid enum value");
-                return 0.25f;
-            }
-        }()};
-    return relative_pos;
-}
+auto make_relative_position(float position, WrapMode wrap_mode) -> RelativePosition;
+
 } // namespace ImGuiGradient
