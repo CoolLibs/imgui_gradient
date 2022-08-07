8ec3bed0e6eb885a961a0a706f3489fa5790fad4
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-07 10:22:50 +0200

    🎨[State]

--- src/State.hpp
+++ src/State.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
 #include "Gradient.hpp"
+
 namespace ImGuiGradient {
+
 namespace internal {
+
 struct State {
     Gradient gradient{};
     Mark*    dragging_mark{};
     Mark*    selected_mark{};
     Mark*    mark_to_hide{};
 };
+
 }} // namespace ImGuiGradient::internal
