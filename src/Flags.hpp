801c33bbd5f61dece54d347e82fed72746c6347d
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-07 11:24:17 +0200

    💡 [Flags] Add Documentation for each flag

--- src/Flags.hpp
+++ src/Flags.hpp
@@ -5,24 +5,24 @@ namespace ImGuiGradient {
 using Flags = int;
 
 namespace Flag {
-// TODO(ASG) Documentation on each flag
+
 enum ImGuiGradientFlag {
-    None                    = 0,
-    NoTooltip               = 1 << 1,
-    NoResetButton           = 1 << 2,
-    NoLabel                 = 1 << 3,
-    NoRandomModeCheckBox    = 1 << 4,
-    NoAddButton             = 1 << 5,
-    NoRemoveButton          = 1 << 6,
-    NoPositionSlider        = 1 << 7,
-    NoColorEdit             = 1 << 8,
-    NoWrapModeSelector      = 1 << 9,
-    NoInterpolationSelector = 1 << 10,
-    NoDragDownToDelete      = 1 << 11,
-    NoBorder                = 1 << 12,
-    NoAddAndRemoveButtons   = NoAddButton | NoRemoveButton,
-    NoMarkOptions           = NoColorEdit | NoPositionSlider,
-    NoSelector              = NoInterpolationSelector | NoWrapModeSelector,
+    None                    = 0,                                            // All Options are enabled
+    NoTooltip               = 1 << 1,                                       // No tooltip when hovering a widget
+    NoResetButton           = 1 << 2,                                       // No button to reset to the default value
+    NoLabel                 = 1 << 3,                                       // No name for gradient widget
+    NoRandomModeCheckBox    = 1 << 4,                                       // No checkbox to enable or disable the random mode when adding a new color
+    NoAddButton             = 1 << 5,                                       // No "+" button to add a mark
+    NoRemoveButton          = 1 << 6,                                       // No "-" button to remove a mark
+    NoPositionSlider        = 1 << 7,                                       // No slider widget to chose a precise position when selecting a mark
+    NoColorEdit             = 1 << 8,                                       // No color edit widget when selecting a mark
+    NoWrapModeSelector      = 1 << 9,                                       // No widget to change wrap mode position
+    NoInterpolationSelector = 1 << 10,                                      // No widget to change how gradient colors are computed
+    NoDragDownToDelete      = 1 << 11,                                      // Don't delete a mark by dragging it down
+    NoBorder                = 1 << 12,                                      // No border around gradient widget
+    NoAddAndRemoveButtons   = NoAddButton | NoRemoveButton,                 // No "+" and "-" buttons
+    NoMarkOptions           = NoColorEdit | NoPositionSlider,               // No new widgets appear when a brand is selected
+    NoSelector              = NoInterpolationSelector | NoWrapModeSelector, // No selector to change wrap mode position and  how gradient colors are computed
 };
 
 } // namespace Flag
