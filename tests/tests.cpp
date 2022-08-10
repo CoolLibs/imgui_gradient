b0b6b7dd6486564b5e818fc0df12a65d7e86084e
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-10 10:25:44 +0200

    🧼 [tests] Add const

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -44,161 +44,160 @@ TEST_CASE("Wrap modes")
 {
     SUBCASE("repeat_position() when position in the range [0,1]")
     {
-        // TODO(ASG) const everywhere
-        float position   = 0.2f;
-        float repeat_pos = Utils::repeat_position(position);
+        const float position   = 0.2f;
+        const float repeat_pos = Utils::repeat_position(position);
 
         CHECK(doctest::Approx(repeat_pos) == 0.2f);
     }
     SUBCASE("repeat_position() when position in the range [-1,0]")
     {
-        float position   = -0.3f;
-        float repeat_pos = Utils::repeat_position(position);
+        const float position   = -0.3f;
+        const float repeat_pos = Utils::repeat_position(position);
 
         CHECK(doctest::Approx(repeat_pos) == 0.7f);
     }
     SUBCASE("repeat_position() when position is < -1")
     {
-        float position   = -1.4f;
-        float repeat_pos = Utils::repeat_position(position);
+        const float position   = -1.4f;
+        const float repeat_pos = Utils::repeat_position(position);
 
         CHECK(doctest::Approx(repeat_pos) == 0.6f);
     }
     SUBCASE("repeat_position() when position > 1")
     {
-        float position   = 1.8f;
-        float repeat_pos = Utils::repeat_position(position);
+        const float position   = 1.8f;
+        const float repeat_pos = Utils::repeat_position(position);
 
         CHECK(doctest::Approx(repeat_pos) == 0.8f);
     }
     SUBCASE("repeat_position() when position just before 1")
     {
-        float position   = .99f;
-        float repeat_pos = Utils::repeat_position(position);
+        const float position   = .99f;
+        const float repeat_pos = Utils::repeat_position(position);
 
         CHECK(doctest::Approx(repeat_pos) == .99f);
     }
     SUBCASE("repeat_position() when position just after 1")
     {
-        float position   = 1.01f;
-        float repeat_pos = Utils::repeat_position(position);
+        const float position   = 1.01f;
+        const float repeat_pos = Utils::repeat_position(position);
 
         CHECK(doctest::Approx(repeat_pos) == .01f);
     }
     SUBCASE("repeat_position() when position just before 0")
     {
-        float position   = -0.01f;
-        float repeat_pos = Utils::repeat_position(position);
+        const float position   = -0.01f;
+        const float repeat_pos = Utils::repeat_position(position);
 
         CHECK(doctest::Approx(repeat_pos) == 0.99f);
     }
     SUBCASE("repeat_position() when position just after 0")
     {
-        float position   = 0.01f;
-        float repeat_pos = Utils::repeat_position(position);
+        const float position   = 0.01f;
+        const float repeat_pos = Utils::repeat_position(position);
 
         CHECK(doctest::Approx(repeat_pos) == 0.01f);
     }
 
     SUBCASE("mirror_clamp_position() when position in the range [0,1]")
     {
-        float position         = 0.7f;
-        float mirror_clamp_pos = Utils::mirror_clamp_position(position);
+        const float position         = 0.7f;
+        const float mirror_clamp_pos = Utils::mirror_clamp_position(position);
 
         CHECK(doctest::Approx(mirror_clamp_pos) == 0.7f);
     }
     SUBCASE("mirror_clamp_position() when position in the range [-1,0]")
     {
-        float position         = -0.4f;
-        float mirror_clamp_pos = Utils::mirror_clamp_position(position);
+        const float position         = -0.4f;
+        const float mirror_clamp_pos = Utils::mirror_clamp_position(position);
 
         CHECK(doctest::Approx(mirror_clamp_pos) == 0.4f);
     }
     SUBCASE("mirror_clamp_position() when position is negative and < -1")
     {
-        float position         = -1.3f;
-        float mirror_clamp_pos = Utils::mirror_clamp_position(position);
+        const float position         = -1.3f;
+        const float mirror_clamp_pos = Utils::mirror_clamp_position(position);
 
         CHECK(doctest::Approx(mirror_clamp_pos) == 1.f);
     }
     SUBCASE("mirror_clamp_position() when position > 1")
     {
-        float position         = 1.8f;
-        float mirror_clamp_pos = Utils::mirror_clamp_position(position);
+        const float position         = 1.8f;
+        const float mirror_clamp_pos = Utils::mirror_clamp_position(position);
 
         CHECK(doctest::Approx(mirror_clamp_pos) == 1.f);
     }
     SUBCASE("mirror_clamp_position() when position = 1")
     {
-        float position         = 1.f;
-        float mirror_clamp_pos = Utils::mirror_clamp_position(position);
+        const float position         = 1.f;
+        const float mirror_clamp_pos = Utils::mirror_clamp_position(position);
 
         CHECK(doctest::Approx(mirror_clamp_pos) == 1.f);
     }
     SUBCASE("mirror_clamp_position() when position = 0")
     {
-        float position         = 0.f;
-        float mirror_clamp_pos = Utils::mirror_clamp_position(position);
+        const float position         = 0.f;
+        const float mirror_clamp_pos = Utils::mirror_clamp_position(position);
 
         CHECK(doctest::Approx(mirror_clamp_pos) == 0.f);
     }
 
     SUBCASE("mirror_repeat_position() when position in the range [0,1]")
     {
-        float position          = 0.4f;
-        float mirror_repeat_pos = Utils::mirror_repeat_position(position);
+        const float position          = 0.4f;
+        const float mirror_repeat_pos = Utils::mirror_repeat_position(position);
 
         CHECK(doctest::Approx(mirror_repeat_pos) == 0.4f);
     }
     SUBCASE("mirror_repeat_position() when position in the range [-1,0]")
     {
-        float position          = -0.2f;
-        float mirror_repeat_pos = Utils::mirror_repeat_position(position);
+        const float position          = -0.2f;
+        const float mirror_repeat_pos = Utils::mirror_repeat_position(position);
 
         CHECK(doctest::Approx(mirror_repeat_pos) == 0.2f);
     }
     SUBCASE("mirror_repeat_position() when position is negative and < -1")
     {
-        float position          = -1.6f;
-        float mirror_repeat_pos = Utils::mirror_repeat_position(position);
+        const float position          = -1.6f;
+        const float mirror_repeat_pos = Utils::mirror_repeat_position(position);
 
         CHECK(doctest::Approx(mirror_repeat_pos) == 0.4f);
     }
     SUBCASE("mirror_repeat_position() when position > 1")
     {
-        float position          = 1.8f;
-        float mirror_repeat_pos = Utils::mirror_repeat_position(position);
+        const float position          = 1.8f;
+        const float mirror_repeat_pos = Utils::mirror_repeat_position(position);
 
         CHECK(doctest::Approx(mirror_repeat_pos) == 0.2f);
     }
     SUBCASE("mirror_repeat_position() when position = 1")
     {
-        float position          = 1.f;
-        float mirror_repeat_pos = Utils::mirror_repeat_position(position);
+        const float position          = 1.f;
+        const float mirror_repeat_pos = Utils::mirror_repeat_position(position);
 
         CHECK(doctest::Approx(mirror_repeat_pos) == 1.f);
     }
     SUBCASE("mirror_repeat_position() when position = 0")
     {
-        float position          = 0.f;
-        float mirror_repeat_pos = Utils::mirror_repeat_position(position);
+        const float position          = 0.f;
+        const float mirror_repeat_pos = Utils::mirror_repeat_position(position);
 
         CHECK(doctest::Approx(mirror_repeat_pos) == 0.f);
     }
 
     SUBCASE("test intermediate functions : modulo(x,mod) when x = 0")
     {
-        float x          = 0.f;
-        float mod        = 2.f;
-        float modulo_res = Utils::modulo(x, mod);
+        const float x          = 0.f;
+        const float mod        = 2.f;
+        const float modulo_res = Utils::modulo(x, mod);
 
         CHECK(doctest::Approx(modulo_res) == 0.f);
     }
     SUBCASE("test intermediate functions : modulo(x, mod) when x = 1")
     {
-        float x          = 1.f;
-        float mod        = 2.f;
-        float modulo_res = Utils::modulo(x, mod);
+        const float x          = 1.f;
+        const float mod        = 2.f;
+        const float modulo_res = Utils::modulo(x, mod);
 
         CHECK(doctest::Approx(modulo_res) == 1.f);
     }
