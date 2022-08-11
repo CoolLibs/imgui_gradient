27f87ef212d04534b4079d5ced549bc1ef272382
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-11 11:12:21 +0200

    🚨 Add constructors in Mark and SurrondingMarks structs to compile with MacOS Clang

--- src/Mark.hpp
+++ src/Mark.hpp
@@ -9,6 +9,9 @@ struct Mark {
     RelativePosition position{0.f};
     ColorRGBA        color{0.f, 0.f, 0.f, 1.f};
 
+    Mark(RelativePosition position, ColorRGBA color) // Need it to compile with MacOS Clang
+        : position{position}, color{color} {};
+
     friend auto operator==(const Mark& a, const Mark& b) -> bool
     {
         return a.position == b.position &&
--- src/SurroundingMarks.hpp
+++ src/SurroundingMarks.hpp
@@ -5,6 +5,8 @@
 namespace ImGuiGradient { namespace internal {
 
 struct SurroundingMarks {
+    SurroundingMarks(const Mark* lower, const Mark* upper) // Need it to compile with MacOS Clang
+        : lower(lower), upper(upper){};
     const Mark* lower{nullptr};
     const Mark* upper{nullptr};
 };
