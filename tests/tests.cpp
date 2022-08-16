227ae444c7adb9fdee443482f6e70ab27db3957a
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-16 16:02:30 +0200

    ✅ [tests] Use widgets from [extras_widgets] + Add a test to enable the random color mode

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,6 +4,7 @@
 #include <quick_imgui/quick_imgui.hpp>
 #include "../generated/checkboxes_for_all_flags.inl"
 #include "../src/Utils.hpp" // to test wrap mode fonctions
+#include "../src/extras_widgets.hpp"
 #include "../src/widgets.hpp"
 
 auto main(int argc, char* argv[]) -> int
@@ -69,16 +70,10 @@ auto main(int argc, char* argv[]) -> int
                     ImGuiColorEditFlags_NoInputs
             );
             static ImGuiGradient::Settings settings{};
-            ImGui::Combo(
-                "Position Mode",
-                reinterpret_cast<int*>(&settings.wrap_mode),
-                " Clamp\0 Repeat\0 Mirror Repeat\0\0"
-            );
-            ImGui::Combo(
-                "Interpolation Mode",
-                reinterpret_cast<int*>(&settings.interpolation_mode),
-                " Linear\0 Constant\0\0"
-            );
+            ImGuiGradient::wrap_mode_selector(settings.wrap_mode, true);
+            ImGuiGradient::gradient_interpolation_mode_selector(settings.interpolation_mode, true);
+            ImGuiGradient::random_mode_box(settings.should_use_a_random_color_for_the_new_marks, true);
+
             ImGui::End();
             ImGui::Begin("imgui_gradient tests");
             settings.flags       = flags;
