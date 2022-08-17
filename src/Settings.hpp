f5447095ad988f5316e6398f3090cd694251197d
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-17 11:54:48 +0200

    💡 [Settings] Add documentation

--- src/Settings.hpp
+++ src/Settings.hpp
@@ -18,10 +18,14 @@ struct Settings {
     ImGuiGradient::Flags flags = ImGuiGradient::Flag::None;
     /// Change color picker behaviour
     ImGuiColorEditFlags color_flags = ImGuiColorEditFlags_None;
-
-    WrapMode      wrap_mode{WrapMode::Clamp};
+    /// Controls how a mark position that is outside of the [0, 1] range is mapped back into that range.
+    WrapMode wrap_mode{WrapMode::Clamp};
+    /// Controls how the colors are interpolated between two marks.
     Interpolation interpolation_mode{Interpolation::Linear};
-    bool          should_use_a_random_color_for_the_new_marks{false};
+    /// Controls how the new mark color is chosen.
+    /// If `should_use_a_random_color_for_the_new_marks` is enable the new mark color will be a random color,
+    /// else it will be the one wich is computed at the mark position.
+    bool should_use_a_random_color_for_the_new_marks{false};
 };
 
 } // namespace ImGuiGradient
