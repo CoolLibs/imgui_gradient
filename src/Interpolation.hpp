72c04eb8a5963ce9566735578e79f985e5792ace
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-20 12:09:46 +0200

    🚨 Fix missing size_t include

--- src/Interpolation.hpp
+++ src/Interpolation.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstddef> // Includes size_t
+
 namespace ImGuiGradient {
 
 /// Controls how the colors are interpolated between two marks.
--- src/WrapMode.hpp
+++ src/WrapMode.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstddef> // Includes size_t
+
 namespace ImGuiGradient {
 
 /// Controls how a position that is outside of the [0, 1] range is mapped back into that range.
